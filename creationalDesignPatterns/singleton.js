1aa540a5215c6a870003f6812c1129c61235506a
Nazar Zubyk <nazarzubyk@gmail.com>
2025-02-10 13:04:05 +0200

    clean up singleton

--- creationalDesignPatterns/singleton.js
+++ creationalDesignPatterns/singleton.js
@@ -42,31 +42,3 @@ globalConfigInstance2.loadConfig(config2);
 
 console.log(' value - ',globalConfigInstance.getConfig());
 
-function MyClass() {
-    // instance fields
-    this.myInstanceField = 'Hello, instance!';
-  }
-  
-  // static fields
-  MyClass.myStaticField = 'Hello, world!';
-  
-  // instance methods
-  MyClass.prototype.sayHello = function() {
-    console.log(`Hello, my name is ${this.myInstanceField}!`);
-  }
-  
-  // update the static field
-  MyClass.myStaticField = 'Updated static field!';
-  
-  // create two instances of the class
-  const myObject1 = new MyClass();
-  const myObject2 = new MyClass();
-  
-  // both instances share the same static field
-  console.log(myObject1.constructor.myStaticField); // outputs "Updated static field!"
-  console.log(myObject2.constructor.myStaticField); // outputs "Updated static field!"
-
-
-  myObject1.myStaticField = 'New static field!';
-  console.log(myObject1.constructor.myStaticField); // outputs "New static field!"
-  console.log(myObject2.constructor.myStaticField); // outputs "New static field!"
